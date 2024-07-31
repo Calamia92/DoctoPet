ba4618b4e18e6bf3e147e6f6410426079db46eb5
Abondance <kazadiabondance@gmail.com>
2024-07-31 15:56:00 +0200

    profile and request page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,20 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './Layout';
 import Appointment from './Apointement';
 import Login from './Login';
-import Signup from './Signup';
+import SignUp from './SignUp';
+import Request from './components/Request';
+import Profile from './components/Profile';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Layout>
         <Routes>
-          <Route path="/signup" element={<Signup/>} />
+          <Route path="/signup" element={<SignUp/>} />
           <Route path="/apointements" element={<Appointment />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/request" element={<Request />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="/" element={<div>Home Page</div>} />
         </Routes>
       </Layout>
